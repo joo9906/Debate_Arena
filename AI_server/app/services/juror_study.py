2da762253376d48f1b6642e2507e76bc51cf52c9
jooyoung <happyboyjoo@naver.com>
2025-10-05 01:43:07 +0900

    미사용 파일 / 함수 정리

--- AI_server/app/services/juror_study.py
+++
@@ -1,7 +0,0 @@
-from transformers import pipeline
-from app.models.schemas import STTRequest, RequestInput, ResponseOutput, LastInput, LastOutput, AttackdefenseInput
-import os
-import httpx
-from dotenv import load_dotenv
-load_dotenv()
-
